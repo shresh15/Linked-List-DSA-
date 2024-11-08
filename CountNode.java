4e9b05bc2103fdac14777b4d1a576b30101d89d3
shrestha <shresthachakrabortykgp@gmail.com>
2024-11-08 12:21:57 +0530

    Node counting :GFG Problems

---
+++ CountNode.java
@@ -0,0 +1,19 @@
+// GFG
+ class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        Node temp=head;
+        int count=1;
+        while(temp.next!=null){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+        
+    }
+}
